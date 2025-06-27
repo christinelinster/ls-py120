ccf74ecc3bca2249d9c4cc85eb9debb71f2afb5b
Christine Lin <lin.christine19@gmail.com>
2025-06-27 13:37:18 -0700

    complete oo_3 exercises

---
+++ oo_basics3/oo_3.8.py
@@ -0,0 +1,20 @@
+# Method Resolution Path (Part 1)
+
+class Animal:
+    def __init__(self, color):
+        self._color = color
+
+    @property
+    def color(self):
+        return self._color
+
+class Cat(Animal):
+    pass
+
+class Bird(Animal):
+    pass
+
+cat1 = Cat('Black')
+print(cat1.color)
+
+# Cat, Animal, object 
---
+++ oo_basics3/oo_3.9.py
@@ -0,0 +1,15 @@
+# Method Resolution Path (Part 2)
+
+class Animal:
+    pass
+
+class Cat(Animal):
+    pass
+
+class Bird(Animal):
+    pass
+
+cat1 = Cat()
+cat1.color
+
+#Cat, Animal, object 
